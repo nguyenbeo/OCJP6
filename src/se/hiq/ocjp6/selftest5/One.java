7a2c561761892e178392c4e129afab637fd86cd9
nguyenbeo <nguyenbeo@gmail.com>
2014-09-18 15:17:34 +0200

    Selftest - Chapter 5 Flow Control, Exception and Assertions

---
+++ src/se/hiq/ocjp6/selftest5/One.java
@@ -0,0 +1,7 @@
+package se.hiq.ocjp6.selftest5;
+
+class One {
+	public static void main (String[] args){
+		int assert = 0;
+	} 
+}
---
+++ src/se/hiq/ocjp6/selftest5/Two.java
@@ -0,0 +1,7 @@
+package se.hiq.ocjp6.selftest5;
+
+class Two {
+	public static void main (String[] args){
+		assert(false);
+	}
+}
